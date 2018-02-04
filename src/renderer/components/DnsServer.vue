ddffd83da1d9e41baad46042b492aef09af5b7ab
Hugh Hoang <hugh.hoang@gmail.com>
2018-02-04 22:36:37 +1300

    DNS server UI

--- src/renderer/components/DnsServer.vue
+++ src/renderer/components/DnsServer.vue
@@ -2,25 +2,53 @@
   <div id="dns-server">
     <v-container grid-list-md>
       <v-layout row wrap>
-        <v-flex xs6>
+        <v-flex md6 sm12>
           <v-card>
             <v-card-title>
-              <h2>Status {{turnOn}}</h2>
+              <h2>Status</h2>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              <v-switch label="ON / OFF" v-model="turnOn" color="success" @change="switchOnOff"></v-switch>
+              <v-switch label="ON / OFF" hide-details v-model="onOff" color="success"></v-switch>
             </v-card-text>
           </v-card>
         </v-flex>
-        <v-flex xs6 disabled>
+        <v-flex md6 sm12>
           <v-card>
             <v-card-title>
               <h2>Settings</h2>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              <v-btn @click="test">test me</v-btn>
+              <v-form>
+              <h3>Internet access</h3>
+               <v-radio-group v-model="settings.internet" column hint="Requests to playstation.net are ALWAYS blocked" persistent-hint>
+                <v-radio :disabled="server.dns" label="No Internet"
+                  color="info"
+                  value="none"></v-radio>
+                <v-radio :disabled="server.dns" label="Lmited Internet access"
+                  color="info"
+                  value="google"></v-radio>
+                </v-radio-group>
+                <v-divider class="mb-4 mt-4"></v-divider>
+                <h3>PS4 exploit host</h3>
+                <v-radio-group v-model="settings.redirect.type">
+                  <v-radio :disabled="server.dns" label="Internal"
+                    color="info"
+                    value="internal"></v-radio>
+                  <v-radio :disabled="server.dns" label="PS4 update blocker (LightningMods &amp; CelesteBlue)"
+                    color="info"
+                    value="ps4henv2"></v-radio>
+                  <v-radio :disabled="server.dns" label="Other"
+                    color="info"
+                    value="other"></v-radio>
+                </v-radio-group>
+                <v-text-field
+                  v-if="settings.redirect.type === 'other'"
+                  label="IP or domain"
+                  v-model="settings.redirect.value"
+                  required></v-text-field>
+              </v-form>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -30,18 +58,31 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   data: () => ({
-    turnOn: true,
+    settings: {
+      internet: 'none',
+      redirect: {
+        type: 'internal',
+        value: '',
+      },
+    },
   }),
-
+  computed: {
+    ...mapState('Layout', ['server']),
+    onOff: {
+      get() {
+        return this.server.dns;
+      },
+      set(status) {
+        this.setServerStatus({ server: 'dns', status });
+      },
+    },
+  },
   methods: {
-    test() {
-      this.turnOn = false;
-    },
-    switchOnOff() {
-      console.log(this.turnOn);
-    },
+    ...mapActions('Layout', ['setServerStatus']),
   },
 };
 </script>
--- src/renderer/store/modules/Layout.js
+++ src/renderer/store/modules/Layout.js
@@ -1,4 +1,4 @@
-import { LAYOUT_TITLE } from '../types';
+import { LAYOUT_TITLE, LAYOUT_SERVER_STATUS } from '../types';
 
 const state = {
   title: 'Dashboard',
@@ -12,12 +12,17 @@ const mutations = {
   [LAYOUT_TITLE]: (state, title) => {
     state.title = title;
   },
+  [LAYOUT_SERVER_STATUS]: (state, { server, status }) => {
+    state.server[server] = status;
+  },
 };
 
 const actions = {
   setTitle: ({ commit }, title) => {
-    // do something async
     commit(LAYOUT_TITLE, title);
+  },
+  setServerStatus: ({ commit }, { server, status }) => {
+    commit(LAYOUT_SERVER_STATUS, { server, status });
   },
 };
 
