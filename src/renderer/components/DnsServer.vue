700322661ec06f3fc18b9dadf866e7ecdb747fb6
Hugh Hoang <hugh.hoang@gmail.com>
2018-02-04 23:33:31 +1300

    Refine DNS server UI

--- src/renderer/components/DnsServer.vue
+++ src/renderer/components/DnsServer.vue
@@ -22,34 +22,38 @@
             <v-card-text>
               <v-form>
               <h3>Internet access</h3>
-               <v-radio-group v-model="settings.internet" column hint="Requests to playstation.net are ALWAYS blocked" persistent-hint>
+               <v-radio-group v-model="settings.internet" column hint="NOTE: Requests to playstation.net are ALWAYS blocked" persistent-hint>
                 <v-radio :disabled="server.dns" label="No Internet"
-                  color="info"
+                  color="primary"
                   value="none"></v-radio>
-                <v-radio :disabled="server.dns" label="Lmited Internet access"
-                  color="info"
+                <v-radio :disabled="server.dns" label="Limited Internet access"
+                  color="primary"
                   value="google"></v-radio>
                 </v-radio-group>
                 <v-divider class="mb-4 mt-4"></v-divider>
                 <h3>PS4 exploit host</h3>
                 <v-radio-group v-model="settings.redirect.type">
-                  <v-radio :disabled="server.dns" label="Internal"
-                    color="info"
+                  <v-radio :disabled="server.dns" label="Built-in"
+                    color="primary"
                     value="internal"></v-radio>
-                  <v-radio :disabled="server.dns" label="PS4 update blocker (LightningMods &amp; CelesteBlue)"
-                    color="info"
-                    value="ps4henv2"></v-radio>
-                  <v-radio :disabled="server.dns" label="Other"
-                    color="info"
+                  <v-radio :disabled="server.dns" label="Custom"
+                    color="primary"
                     value="other"></v-radio>
                 </v-radio-group>
                 <v-text-field
+                  :disabled="server.dns"
                   v-if="settings.redirect.type === 'other'"
                   label="IP or domain"
                   v-model="settings.redirect.value"
                   required></v-text-field>
               </v-form>
             </v-card-text>
+            <v-snackbar
+              :top="true"
+              :color="snackbar.colour"
+              v-model="snackbar.show">{{ snackbar.text }}
+              <v-btn dark flat @click.native="snackbar.show = false">Close</v-btn>
+            </v-snackbar>
           </v-card>
         </v-flex>
       </v-layout>
@@ -69,7 +73,22 @@ export default {
         value: '',
       },
     },
+    snackbar: {
+      show: false,
+      text: '',
+    },
   }),
+  created() {
+    this.checkValidHost = host => {
+      const domain = /^(?=.{0,253}$)(([a-z0-9][a-z0-9-]{0,61}[a-z0-9]|[a-z0-9])\.)+((?=.*[^0-9])([a-z0-9][a-z0-9-]{0,61}[a-z0-9]|[a-z0-9]))$/i;
+      const digit = /^\d+$/;
+      const ip = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+      const hostString = host.replace(/\./g, '');
+
+      return digit.test(hostString) ? ip.test(host) : domain.test(host);
+    };
+  },
   computed: {
     ...mapState('Layout', ['server']),
     onOff: {
@@ -77,7 +96,28 @@ export default {
         return this.server.dns;
       },
       set(status) {
-        this.setServerStatus({ server: 'dns', status });
+        this.snackbar.colour = 'success';
+
+        if (status) {
+          if (
+            this.settings.redirect.type === 'other' &&
+            !this.checkValidHost(this.settings.redirect.value)
+          ) {
+            this.snackbar.text =
+              'Cannot start server. Invalid IP or domain for custom host.';
+            this.snackbar.colour = 'error';
+          } else {
+            this.snackbar.text = 'DNS Server has started successfully.';
+          }
+        } else {
+          this.snackbar.text = 'DNS Server has stopped successfully.';
+        }
+
+        this.snackbar.show = true;
+
+        if (this.snackbar.colour === 'success') {
+          this.setServerStatus({ server: 'dns', status });
+        }
       },
     },
   },
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,11 +6,18 @@ import App from './App';
 import router from './router';
 import store from './store';
 
-
 import '../../node_modules/material-icon-font/iconfont/material-design-iconfont.css';
 import '../../node_modules/vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#4fc3f7',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c',
+    success: '#388e3c',
+  },
+});
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
